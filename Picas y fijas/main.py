958801dd836662d4dff50feb9bbcbf3fd919c19c
C0C045 <St2ven.cabarcas@gmail.com>
2020-11-16 15:58:22 -0500

    Picas y fijas terminado

--- Picas y fijas/main.py
+++ Picas y fijas/main.py
@@ -1,6 +1,7 @@
 #Pico y Fijas
 import random
 
+#Declaración de variables
 w = 1
 contador = 0
 lista = []
@@ -14,6 +15,7 @@ numeros_three = []
 numeros_four = []
 numeros_five = []
 
+#Método para validar nombre en mi documento con respecto a n dígitos
 def validar_nombre(nombre,n):
   s = establecer_archivo("r")
   for linea in s.readlines():
@@ -22,14 +24,17 @@ def validar_nombre(nombre,n):
       print("Usted ya esta registrado para",n,"cifras")
       exit()
 
+#Manejo del archivo y su modificador
 def establecer_archivo(permiso):
   arch = open("archivo.txt", permiso)
   return arch
 
+#Metodo para imprimir el lider
 def imprimir_lider(lider):
   s = establecer_archivo("a")
   s.write(lider+"\n")
 
+#Hacer la respectiva comprobación para comprobar el líder
 def comprobar_lider():
   s = establecer_archivo("r")
   lideres_three = []
@@ -38,7 +43,7 @@ def comprobar_lider():
   for linea in s.readlines():
     elementos = [str(x) for x in linea.split(",")]
     if len(elementos)==6:
-      #3 CIFRAS
+      #Líderes para 3 cifras
       if int(elementos[-1]) == 3 and int(elementos[1]) == 3:
         list_three.append(elementos[0]+","+elementos[2])
         numeros_three.append(elementos[2])
@@ -49,7 +54,7 @@ def comprobar_lider():
           if (elementos1[-1] == minimo):
             lideres_three.append(elementos1[0])
             lideres_three.append("-")
-      #4 CIFRAS
+      #Líderes para 4 cifras
       elif int(elementos[-1]) == 4 and int(elementos[1]) == 4:
         list_four.append(elementos[0]+","+elementos[2])
         numeros_four.append(elementos[2])
@@ -60,7 +65,7 @@ def comprobar_lider():
           if (elementos2[-1] == minimo):
             lideres_four.append(elementos2[0])
             lideres_four.append("-")
-      #5 CIFRAS
+      #Lideres para 5 cifras
       elif int(elementos[-1]) == 5 and int(elementos[1]) == 5:
         list_five.append(elementos[0]+","+elementos[2])
         numeros_five.append(elementos[2])
@@ -71,15 +76,17 @@ def comprobar_lider():
           if (elementos3[-1] == minimo):
             lideres_five.append(elementos3[0])
             lideres_five.append("-")
-  imprimir_lider("LIDERES DE 3 CIFRAS:"+str(lideres_three))
-  imprimir_lider("LIDERES DE 4 CIFRAS:"+str(lideres_four))
-  imprimir_lider("LIDERES DE 5 CIFRAS:"+str(lideres_five)+"\n")
   print("LIDERES DE 3 CIFRAS:",*lideres_three)
   print("LIDERES DE 4 CIFRAS:",*lideres_four)
   print("LIDERES DE 5 CIFRAS:",*lideres_five)
-       
+  """imprimir_lider("LIDERES DE 3 CIFRAS:"+str(lideres_three))
+  imprimir_lider("LIDERES DE 4 CIFRAS:"+str(lideres_four))
+  imprimir_lider("LIDERES DE 5 CIFRAS:"+str(lideres_five)+"\n")"""
+  
+#Pedir el nombre del usuario
 nombre = input("Ingrese su nombre:")
 
+#Menu
 print("===OPCIONES DE JUEGO===")
 print("1) 3 Digitos - 10 intentos para ganar")
 print("2) 4 Digitos - 15 intentos para ganar")
@@ -89,6 +96,7 @@ menu = int(input("Elija una opción de juego:"))
 
 n = menu+2
 
+#Validar el número que no se repita
 def validar_numero(numero):
   if len(numero) == 1:
     v =  True
@@ -99,12 +107,14 @@ def validar_numero(numero):
       v = validar_numero(numero[1:])
   return v
 
+#Generar el numero aleatorio
 while w<=n:
   num = random.randint(0,9)
   if num not in lista:
     lista.append(num)
     w+=1
 
+#Asignar el numero de intentos dependiendo de la opcion marcada
 if(menu==1):
   inten=10
   validar_nombre(nombre,n)
@@ -115,6 +125,7 @@ elif menu == 3:
   inten=20
   validar_nombre(nombre,n)
 
+#Ir digitando los valores e ir guardadndo en el documento
 for i in range(0,inten):
   f = establecer_archivo("a")
   if(contador<n):
@@ -134,6 +145,7 @@ for i in range(0,inten):
     cont_fijas = 0
     cont_pica = 0
     lista1 = []
+    #Hacer la comprobación para saber las picas y fijas
     for i in range(0,n):
       for j in range(0,n):
         lista1.append(str(inte)[j])
@@ -146,6 +158,7 @@ for i in range(0,inten):
           cont_pica+=1
     f.write(str(cont_pica)+","+str(cont_fijas)+"\n")
 
+#Validar si el usuario ganó o perdió
 if (contador == n):
   print("¡USTED HA ADIVINADO EL NÚMERO CORRECTAMENTE!")
   print("EL NÚMERO ES: ",*lista,"\n")
@@ -154,6 +167,5 @@ else:
   print("¡USTED PERDIO!")
   print("EL NÚMERO ERA: ",*lista)
 
-#Leer el documeto y decir quien es el lider
 com = comprobar_lider()
 f.close()
